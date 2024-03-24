6d39fa1c8eff6d8e07fb977342b70c688b846d6b
NesoDev <ever.avendano@unmsm.edu.pe>
2024-03-24 05:38:34 -0500

    improve drawing performance project_2

--- project_2/main.cpp
+++ project_2/main.cpp
@@ -2,73 +2,123 @@
 #include <SDL2/SDL.h>
 
 const float WIDTH = 600.0f, HEIGHT = 600.0f;
+const 
 
-class Object {
+class Particle {
 private:
-    float position[2]; // X (x, y) = (x, y)
-    float velocity[2]; // v (i, j) = vi + vj
+    float position[2]; 
+    float velocity[2];
     float range_x[2];
     int radius;
-    int color[4]; // color = {r, g, b, a}
+    int* color;
+    bool needsRedraw = true;
 public:
-    Object(float x, float y, float v_x, float v_y, int r, int* c) {
+    Particle(int x, int y, float v_x, float v_y, int r, int* c) : needsRedraw(true) {
         position[0] = x;
         position[1] = y;
-
         velocity[0] = v_x;
         velocity[1] = v_y;
-
         range_x[0] = x - r;
         range_x[1] = x + r;
-
         radius = r;
-
+        color = new int[4];
         color[0] = c[0];
         color[1] = c[1];
         color[2] = c[2];
         color[3] = c[3];
     };
 
-    void draw(SDL_Renderer *renderer) {
-        int rectX = static_cast<int>(position[0] - radius);
-        int rectY = static_cast<int>(position[1] - radius);
-        int rectW = static_cast<int>(radius * 2);
-        int rectH = static_cast<int>(radius * 2);
+    ~Particle() {
+        delete[] color;
+    }
 
-        SDL_Rect rect = { rectX, rectY, rectW, rectH };
-        SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
-        SDL_RenderFillRect(renderer, &rect);
-    };
+    void setNeedsRedraw(bool redraw) {
+        needsRedraw = redraw;
+    }
+
+    void setPosition(float x, float y) {
+        position[0] = x;
+        position[1] = y;
+
+        needsRedraw = true;
+    }
+
+    void draw(SDL_Renderer *renderer) {
+        if (needsRedraw) {
+            std::cout << "Particle drawed" << std::endl;
+            SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
+            int x_int = static_cast<int>(position[0]);
+            int y_int = static_cast<int>(position[1]);
+            int x = 0;
+            int y = radius;
+            int d = 3 - 2 * radius;
+
+            while (x <= y) {
+                SDL_RenderDrawPoint(renderer, x_int + x, y_int + y);
+                SDL_RenderDrawPoint(renderer, x_int - x, y_int + y);
+                SDL_RenderDrawPoint(renderer, x_int + x, y_int - y);
+                SDL_RenderDrawPoint(renderer, x_int - x, y_int - y);
+                SDL_RenderDrawPoint(renderer, x_int + y, y_int + x);
+                SDL_RenderDrawPoint(renderer, x_int - y, y_int + x);
+                SDL_RenderDrawPoint(renderer, x_int + y, y_int - x);
+                SDL_RenderDrawPoint(renderer, x_int - y, y_int - x);
+
+                if (d < 0) {
+                    d += 4 * x + 6;
+                } else {
+                    d += 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+            }
+
+            needsRedraw = false;
+        }
+    }
 };
 
-int main( int argc, char *argv[] ) {
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
+int main(int argc, char *argv[]) {
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
     SDL_Event event;
 
-    SDL_Init( SDL_INIT_VIDEO );
+    int num_particles = 20;
+    Particle **particles = new Particle* [num_particles];
+
+    SDL_Init(SDL_INIT_VIDEO);
     bool running = true;
-    SDL_CreateWindowAndRenderer( WIDTH, HEIGHT, 0, &window, &renderer );
+    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer);
 
-    SDL_GetRenderDrawColor( renderer, 0, 0, 0, 0 );
+    SDL_GetRenderDrawColor(renderer, 0, 0, 0, 0);
 
-    if ( window == NULL ) {
+    if (window == NULL) {
         std::cout << "Could not create window: " << SDL_GetError() << std::endl;
         return 1;
     }
+
+    Particle p_1(WIDTH / 2.0f, HEIGHT / 2.0f, 0.0f, 0.0f, 1, new int[4]{184, 253, 113, 1});
+    Particle p_2(WIDTH / 4.0f, HEIGHT / 4.0f, 0.0f, 0.0f, 180, new int[4]{184, 253, 113, 1});
 
     while (running) {
         if (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)) {
                 running = false;
+            } else if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+                p_1.setPosition(event.window.data1 / 2.0f, event.window.data2 / 2.0f);
+                p_2.setPosition(event.window.data1 / 4.0f, event.window.data2 / 4.0f);
+                std::cout << "Ventana Redimensionada" << std::endl;
             }
         }
 
-        int color[4] = {255, 255, 255, 255};
-        Object o_1(WIDTH / 2.0f, HEIGHT / 2.0f, 0.0f, 0.0f, 20, color);
-        o_1.draw(renderer);
+        p_1.draw(renderer);
+        p_2.draw(renderer);
 
         SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
